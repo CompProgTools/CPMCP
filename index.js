f15a5f7a5d2c21a6bb5ef4243c4c9cedcd1c8dc1
Kashyap Sukshavasi <ksukshavasi@gmail.com>
2025-07-11 16:25:34 -0700

    basic setup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
+
+const server = new McpServer({
+  name: "CPMCP",
+  version: "1.0.0"
+});
+
+const inputSchema = {
+  testcases: z.string().describe("The number of testcases and if possible the name of the .txt file")
+};
+
+const transport = new StdioServerTransport();
+await server.connect(transport);
